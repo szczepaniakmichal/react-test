765266b5ce1fdc31f6893c0ffe356fdbf48f60ff
szczepaniakmichal <mssk87@gmail.com>
2020-09-22 21:50:41 +0200

    test useState()

--- src/App.js
+++ src/App.js
@@ -4,15 +4,47 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Witaj w aplikacji testowej</h1>
+const App = () => {
+    const [item] = React.useState({
+        item1: 'kot',
+        item2: 'mysz',
+        item3: 'nietoperz',
+    })
 
-        <FontAwesomeIcon icon={faStar} />
+    const ItemList = ({name}) => (
+        <li>{name}</li>
+    )
 
-    </div>
-  );
+    return (
+        <>
+            <FontAwesomeIcon icon={faStar} />
+            <ul>
+                <ItemList name={item.item1} />
+                <ItemList name={item.item2} />
+                <ItemList name={item.item3} />
+            </ul>
+        </>
+    )
 }
 
 export default App;
+
+
+
+
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+// function App() {
+// return (
+//   <div className="App">
+//       <h1>Witaj w aplikacji testowej</h1>
+//
+//       <FontAwesomeIcon icon={faStar} />
+//
+//
+//
+//   </div>
+// );
+// }
