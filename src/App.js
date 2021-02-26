cbca329be2e9ec092ff4992d65b5a9cc5e2204cd
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-02-26 11:55:13 +0100

    test "react-id-swiper": "^4.0.0",

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+
+import Gallery from "./Gallery";
+
 import './App.css';
 
 
 const App = () => {
-    return (
-        <>
-            <div className="total-price">
-                <div className="total-price__flex">
-                    <div
-                        className="total-price__subtotal">Subtotal: <span
-                        className="total-price__price">$15.67</span> (4 items)
-                    </div>
-                    <div className="total-price__btn">
-                        <button className="total-price__btn-btn" data-test="bundle-slot-add-to-cart">Add
-                            all 4 to cart
-                        </button>
-                    </div>
-                </div>
-            </div>
 
-        </>
+    return (
+      <>
+      <Gallery/>
+      </>
     )
 }
 
