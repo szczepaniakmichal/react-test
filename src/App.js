7f152a79b72183ebca231273042f778fa21c8946
szczepaniakmichal <mssk87@gmail.com>
2021-02-27 10:38:31 +0100

    main store - use createContext from React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom'
 
-import Gallery from "./Gallery";
+import Gallery from './Gallery';
 
-import { AppContext, contextBbj } from "./context/AppContext";
+// import { AppContext, contextBbj } from "./context/AppContext";
+import AppProvider from './context/AppContext';
 
 import './App.css';
 
 
-const App = () => {
-    const [url, setUrl] = useState('localhost')
-    const toggleUrl = () => {
-        setUrl(url === 'localhost' ? 'testbox' : 'localhost')
-    }
+const App = () => (
+    <div>
+        <AppProvider
+            //     value={{
+            //     url,
+            //     toggleUrl
+            // }}
+        >
+            <Gallery />
+        </AppProvider>
+    </div>
+)
+    // const [url, setUrl] = useState('localhost')
+    // const toggleUrl = () => {
+    //     setUrl(url === 'localhost' ? 'testbox' : 'localhost')
+    // }
 
-    return (
-      <AppContext.Provider value={{
-          url,
-          toggleUrl
-      }}>
-      <Gallery />
-      </AppContext.Provider>
-    )
-}
 
 
 export default App;
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,8 +1,29 @@
-import { createContext} from "react";
+import React, { createContext, useState } from "react";
 
-export const contextBbj = {
-    url: 'localhost',
-    toggleUrl: () => {}
-};
+// export const contextBbj = {
+//     url: 'localhost',
+//     toggleUrl: () => {}
+// };
 
-export const AppContext = createContext(contextBbj);
+// export const AppContext = createContext(contextBbj);
+export const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+
+    const [url, setUrl] = useState('localhost')
+    const toggleUrl = () => {
+        setUrl(url === 'localhost' ? 'testbox' : 'localhost')
+    }
+
+
+    return (
+        <AppContext.Provider value={{
+            url,
+            toggleUrl
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppProvider;
