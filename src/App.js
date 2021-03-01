b6585f28f0c75b1907369d27a9f3c9948c9396e4
szczepaniakmichal <mssk87@gmail.com>
2021-03-01 22:52:31 +0100

    add "BrowserRouter as Router" this entry say: BrowserRouter call me Router / import Link / import Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import { Home } from './components/route/home'
+import { MyProject } from "./components/route/myProject";
+import { Contact } from "./components/route/contact";
 
 // i18n translations might still be loaded by the http backend
 // use react's Suspense
