fb563fdf3dffeda2e22d5ba0e6ff33c200f39691
szczepaniakmichal <mssk87@gmail.com>
2021-03-01 22:20:13 +0100

    clean before react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React, { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
-
-import { LanguageSelector } from './components/translation/LanguageSelector'
-
-function MyComponent() {
-    const { t, i18n } = useTranslation();
-
-    return <h1>{t('title')}</h1>
-}
 
 // i18n translations might still be loaded by the http backend
 // use react's Suspense
 export default function App() {
     return (
-        <Suspense fallback="loading">
-            <LanguageSelector />
-            <MyComponent />
-        </Suspense>
+       <div>route</div>
     );
 }
