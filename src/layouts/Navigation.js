c8738535936595cbe415ed9eeb49f9f9f04098f0
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-03-03 23:10:25 +0100

    add Navigation

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { BrowserRouter as Route, NavLink } from "react-router-dom"
+
+// import "../sass/Navigation.scss"
+
+const pagesList = [
+    { name: "start", path: '/', exact: true },
+    { name: "product", path: '/product' },
+    { name: "contact", path: '/contact' },
+    { name: "panel admina", path: '/admin' },
+];
+
+const nav = pagesList.map(page => (
+    <li key={page.name}>
+        <NavLink to={page.path}
+                 exact={page.exact ? page.exact : false}
+        >{page.name}</NavLink>
+    </li>
+));
 
 export const Navigation = () => (
-    <div>Navigation</div>
+    <Route>
+        <nav className="main">
+            <ul>
+                {nav}
+            </ul>
+        </nav>
+    </Route>
 )
