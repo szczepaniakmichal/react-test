e3db69bf69030f2944063aa6b6adffbdc19654ca
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-03-03 23:11:30 +0100

    add sass / variables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import './sass/app.scss'
 import { Navigation } from "./layouts/Navigation";
 import { Page } from "./layouts/Page";
 import { Header } from "./layouts/Header";
