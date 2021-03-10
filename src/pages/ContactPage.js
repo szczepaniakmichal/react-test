8db005cb2d3dbef132253e2deadd78dbd05a8255
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-03-10 20:17:37 +0100

    ContactPage - add form / handlers / css

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const ContactPage = () => (
-    <p>Contact Page</p>
-)
+export const ContactPage = () => {
+    const [value, setValue] = useState('');
+
+    const handleChangeValue = (e) => {
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setValue('');
+    }
+
+    return (
+        <>
+            <div className="contact">
+                <form onSubmit={handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea value={value}
+                              onChange={handleChangeValue}
+                              placeholder="wpisz wiadomość"
+                    />
+                    <button>Wyślij</button>
+                </form>
+            </div>
+        </>
+    )
+}
