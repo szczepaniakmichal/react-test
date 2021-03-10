1a3294142174e2a7aeaa13576a81c2f8d2179395
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-03-10 18:56:24 +0100

    add handle error in header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
 
+import { Error } from '../components/route/Error'
 import { Img } from './Img';
 
 import img0 from '../images/pexels-photo-1.jpeg';
@@ -9,13 +10,11 @@ import img2 from '../images/pexels-photo-3.jpeg';
 import img3 from '../images/pexels-photo-4.jpeg';
 
 export const Header = () => (
-    <>
-        <Switch>
-            <Route path="/" exact render={() => <img src={img0} alt='img0'/>}/>
-            <Route path="/product" render={() => <img src={img1} alt='img1'/>}/>
-            <Route path="/contact" render={() => <img src={img2} alt='img2'/>}/>
-            <Route path="/admin" render={() => <img src={img3} alt='img0'/>}/>
-            <Route render={() => <img src={img0} alt='img3'/>}/>
-        </Switch>
-    </>
+    <Switch>
+        <Route path="/" exact render={() => <img src={img0} alt='img0'/>}/>
+        <Route path="/product" render={() => <img src={img1} alt='img1'/>}/>
+        <Route path="/contact" render={() => <img src={img2} alt='img2'/>}/>
+        <Route path="/admin" render={() => <img src={img3} alt='img0'/>}/>
+        <Route render={() => <Error/>}/>
+    </Switch>
 )
