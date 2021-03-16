7c783313c9ad77c48c403c6f87bb6c082cffe56c
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-03-16 14:19:13 +0100

    test connect inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -24,24 +24,24 @@ export default function App() {
         console.log("e value", e.target.value);
 
         if (nameClassUp) {
-            setMeter( value + 1);
+            setMeter(value + 1);
             setPallet(Math.ceil((value + 1) / 60));
         }
-        if ( nameClassDown ){
+        if (nameClassDown) {
             if (value <= 0) return;
-            setMeter( value - 1);
+            setMeter(value - 1);
             setPallet(Math.ceil((value - 1) / 60));
         }
 
         if (e.target.localName === 'input') {
             if (e.target.value > meter) {
-                setMeter( value);
+                setMeter(value);
                 setPallet(Math.ceil((value) / 60));
             }
 
             if (e.target.value <= meter) {
                 // if (value < 0) return;
-                setMeter( value);
+                setMeter(value);
                 setPallet(Math.ceil((value) / 60));
             }
         }
@@ -59,12 +59,12 @@ export default function App() {
         console.log("e value", e.target.value);
 
         if (nameClassUp) {
-            setMeter( (value + 1) * 60);
+            setMeter((value + 1) * 60);
             setPallet(value + 1);
         }
-        if ( nameClassDown ){
+        if (nameClassDown) {
             if (value <= 0) return;
-            setMeter( (value - 1) * 60);
+            setMeter((value - 1) * 60);
             setPallet(value - 1);
         }
 
@@ -75,7 +75,7 @@ export default function App() {
             }
 
             if (e.target.value <= pallet) {
-                setMeter( value * 60);
+                setMeter(value * 60);
                 setPallet(value);
             }
         }
@@ -113,76 +113,76 @@ export default function App() {
     return (
         <>
             <ul>
-            <li>
-                <div className="group-name-wrapper">
-                    <p>
-                        {"Firerock"} {"30"}
-                    </p>
-                    <button
-                    >
-                        Rabat końcowy
-                    </button>
-                </div>
-                <div className="counters-wrapper">
-                    <div className="counter-wrapper">
-                        <label>Liczba produktów: 444 metry</label>
-                        <div className="counter-input-wrapper meter">
-                            <div className="number-wrapper">
-            <span className="up"
-onClick={(e) => changeMeterToPallet(e)}
-            />
-                                <input
-                                    min="0"
-                                    value={meter}
-                                    // value={state.meter}
-                                    type="number"
-                                    name="meterUp"
-                                    onChange={(e) => {
-                                        console.log("zmiana metrów")
-                                        changeMeterToPallet(e)
-                                    }
-                                    }
-                                />
-                                <span className="down"
-                                      onClick={(e) => changeMeterToPallet(e)}
-                                />
+                <li>
+                    <div className="group-name-wrapper">
+                        <p>
+                            {"Firerock"} {"30"}
+                        </p>
+                        <button
+                        >
+                            Rabat końcowy
+                        </button>
+                    </div>
+                    <div className="counters-wrapper">
+                        <div className="counter-wrapper">
+                            <label>Liczba produktów: 444 metry</label>
+                            <div className="counter-input-wrapper meter">
+                                <div className="number-wrapper">
+                                    <span className="up arrow-up"
+                                          onClick={(e) => changeMeterToPallet(e)}
+                                    />
+                                    <input
+                                        min="0"
+                                        value={meter}
+                                        // value={state.meter}
+                                        type="number"
+                                        name="meterUp"
+                                        onChange={(e) => {
+                                            console.log("zmiana metrów")
+                                            changeMeterToPallet(e)
+                                        }
+                                        }
+                                    />
+                                    <span className="down arrow-down"
+                                          onClick={(e) => changeMeterToPallet(e)}
+                                    />
 
-                            </div>
-                            <div className="counter-name-wrapper">
-                                {/*<span>{material.productVolume}</span>*/}
-                                <span>m<sup>2</sup></span>
+                                </div>
+                                <div className="counter-name-wrapper">
+                                    {/*<span>{material.productVolume}</span>*/}
+                                    <span>m<sup>2</sup></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="counter-wrapper">
-                        <label>Liczba produktów: 333 palety</label>
-                        <div className="counter-input-wrapper pallet">
-                            <div className="number-wrapper">
+                        <div className="counter-wrapper">
+                            <label>Liczba produktów: 333 palety</label>
+                            <div className="counter-input-wrapper pallet">
+                                <div className="number-wrapper">
             <span className="up"
                   onClick={(e) => changePalletToMeter(e)}
             />
-                                <input
-                                    min="0"
-                                    value={pallet}
-                                    // value={state.pallet}
-                                    type="number"
-                                    onChange={(e) => {
-                                        console.log("zmiana palety")
-                                        changePalletToMeter(e)
-                                    }
-                                    }
-                                />
-                                <span className="down"
-                                      onClick={(e) => changePalletToMeter(e)}
-                                />
+                                    <input
+                                        min="0"
+                                        value={pallet}
+                                        // value={state.pallet}
+                                        type="number"
+                                        onChange={(e) => {
+                                            console.log("zmiana palety")
+                                            changePalletToMeter(e)
+                                        }
+                                        }
+                                    />
+                                    <span className="down"
+                                          onClick={(e) => changePalletToMeter(e)}
+                                    />
+                                </div>
+
                             </div>
-
                         </div>
-                    </div>
 
-                </div>
-            </li>
+                    </div>
+                </li>
             </ul>
-            </>
+        </>
     );
 }
