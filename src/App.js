6bd3cae4c810217b0367ca47d6687135d099a320
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-04-07 10:29:57 +0200

    fix import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Provider } from "react-redux";
+// import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
-import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
+import store from "./store/store";
+// import Form from "./Form";
+import FormConsumer from "./Form";
+// import List from "./List"
+import ListConsumer from "./List"
 
 import './App.css';
 import './sass/app.scss'
 
 export default function App() {
     return (
-        <>
-<p>tere fere</p>
-        </>
+        <Provider store={store}>
+            <FormConsumer />
+            <p>Programowanie :)</p>
+            <ListConsumer />
+        </Provider>
     );
 }
