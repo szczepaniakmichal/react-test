9af88330231699f50ce0ccd30f28ec058eb9ced9
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2021-04-20 11:42:46 +0200

    redux - useDispatch - two version

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,23 @@
+// second version
+
 import React, { useState } from "react";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { addRate, editRate } from "./actions/appActions";
 
 const Form = ({
-                    addRate,
-                    author = '',
-                    callback,
-                    editRate,
-                    comment = '',
-                    id,
-                    rate = 0,
+                  author = '',
+                  callback,
+                  comment = '',
+                  id,
+                  rate = 0,
               }) => {
 
     const [authorInput, setAuthorInput] = useState(author);
     const [rateInput, setRateInput] = useState(rate);
     const [commentInput, setCommentInput] = useState(comment);
+
+    const dispatch = useDispatch();
 
     const buttonText = id ? 'Edycja oceny' : 'Dodaj ocene';
 
@@ -35,7 +37,7 @@ const Form = ({
 
         console.log(rateObject);
 
-        id ? editRate(rateObject) : addRate(rateObject);
+        id ? dispatch(editRate(rateObject)) : dispatch(addRate(rateObject));
 
         if (id) {
             callback();
@@ -73,11 +75,93 @@ const Form = ({
     );
 };
 
-const connectActionsToProps = ({
-    addRate,
-    editRate
-})
 
-const FormConsumer = connect(null, connectActionsToProps)(Form)
+export default Form;
 
-export default FormConsumer;
+
+
+// first version
+
+// import React, { useState } from "react";
+// import { connect } from "react-redux";
+//
+// import { addRate, editRate } from "./actions/appActions";
+//
+// const Form = ({
+//                     addRate,
+//                     author = '',
+//                     callback,
+//                     editRate,
+//                     comment = '',
+//                     id,
+//                     rate = 0,
+//               }) => {
+//
+//     const [authorInput, setAuthorInput] = useState(author);
+//     const [rateInput, setRateInput] = useState(rate);
+//     const [commentInput, setCommentInput] = useState(comment);
+//
+//     const buttonText = id ? 'Edycja oceny' : 'Dodaj ocene';
+//
+//     const handleChangeAuthor = (e) => setAuthorInput(e.target.value);
+//     const handleChangeRate = (e) => setRateInput(e.target.value);
+//     const handleChangeComment = (e) => setCommentInput(e.target.value);
+//     const handleOnSubmit = (e) => {
+//
+//         e.preventDefault();
+//         // if (!authorInput.length) return;
+//         const rateObject = {
+//             author: authorInput,
+//             comment: commentInput,
+//             id,
+//             rate: Number(rateInput),
+//         }
+//
+//         console.log(rateObject);
+//
+//         id ? editRate(rateObject) : addRate(rateObject);
+//
+//         if (id) {
+//             callback();
+//         }
+//     }
+//
+//     return (
+//         <form action="" onSubmit={handleOnSubmit}>
+//             <div>
+//                 <label htmlFor="">Autor
+//                     <input type="text"
+//                            value={authorInput}
+//                            onChange={handleChangeAuthor}
+//                     />
+//                 </label>
+//             </div>
+//             <div>
+//                 <label htmlFor="">Ocena
+//                     <input type="number"
+//                            value={rateInput}
+//                            onChange={handleChangeRate}
+//                     />
+//                 </label>
+//             </div>
+//             <div>
+//                 <label htmlFor="">Opinia
+//                     <input type="text"
+//                            value={commentInput}
+//                            onChange={handleChangeComment}
+//                     />
+//                 </label>
+//             </div>
+//             <button type="submit">{buttonText}</button>
+//         </form>
+//     );
+// };
+//
+// const connectActionsToProps = ({
+//     addRate,
+//     editRate
+// })
+//
+// const FormConsumer = connect(null, connectActionsToProps)(Form)
+//
+// export default FormConsumer;
